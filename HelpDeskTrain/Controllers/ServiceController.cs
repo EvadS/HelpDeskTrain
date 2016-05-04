ea29e3600c36e6424988a34dd851606158cf3256
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-04 16:41:59 +0300

    создал справочник для отделов

---
+++ HelpDeskTrain/Controllers/ServiceController.cs
@@ -0,0 +1,45 @@
+﻿using HelpDeskTrain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpDeskTrain.Controllers
+{
+     [Authorize(Roles = "Администратор")]
+    public class ServiceController : Controller
+    {
+
+        HelpdeskContext db = new HelpdeskContext();
+
+        [HttpGet]
+        public ActionResult Departments()
+        {
+            ViewBag.Departments = db.Departments;
+            return View();
+        }
+
+        //Добавление отдела с последующим их отображением
+        [HttpPost]
+        public ActionResult Departments(Department depo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departments.Add(depo);
+                db.SaveChanges();
+            }
+            ViewBag.Departments = db.Departments;
+            return View(depo);
+        }
+        // Удаление отдела по id
+        public ActionResult DeleteDepartment(int id)
+        {
+            Department depo = db.Departments.Find(id);
+            db.Departments.Remove(depo);
+            db.SaveChanges();
+            return RedirectToAction("Departments");
+        }
+
+    }
+}
