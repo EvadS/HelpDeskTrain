fa61a108d259f913107abeaacf70a7407f0ed098
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-04 15:48:19 +0300

    реализовал добавление пользователей

--- HelpDeskTrain/Controllers/UserController.cs
+++ HelpDeskTrain/Controllers/UserController.cs
@@ -22,5 +22,39 @@ namespace HelpDeskTrain.Controllers
             return View(users);
         }
 
+        [HttpGet]
+        [Authorize(Roles = "Администратор")]
+        public ActionResult Create()
+        {
+            //списке мы будем видеть значения свойств Name, но при выборе фактически будут выбираться значения свойств Id.
+            SelectList departments = new SelectList(db.Departments, "Id", "Name");
+            ViewBag.Departments = departments;
+
+            SelectList roles = new SelectList(db.Roles, "Id", "Name");
+            ViewBag.Roles = roles;
+           
+            return View();
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = "Администратор")]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            SelectList departments = new SelectList(db.Departments, "Id", "Name");
+            ViewBag.Departments = departments;
+            SelectList roles = new SelectList(db.Roles, "Id", "Name");
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
     }
 }
