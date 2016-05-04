1ac83fbf371d961f07cc12ea99a54aab923437d8
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-04 16:54:04 +0300

    сделал редактирование и добавление категорий

--- HelpDeskTrain/Controllers/ServiceController.cs
+++ HelpDeskTrain/Controllers/ServiceController.cs
@@ -76,5 +76,34 @@ namespace HelpDeskTrain.Controllers
             return RedirectToAction("Activ");
         }
 
+        // отображение категорий
+        [HttpGet]
+        public ActionResult Categories()
+        {
+            ViewBag.Categories = db.Categories;
+            return View();
+        }
+
+        // Добавление категорий
+        [HttpPost]
+        public ActionResult Categories(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(cat);
+                db.SaveChanges();
+            }
+            ViewBag.Categories = db.Categories;
+            return View(cat);
+        }
+        // Удаление категории по id
+        public ActionResult DeleteCategory(int id)
+        {
+            Category cat = db.Categories.Find(id);
+            db.Categories.Remove(cat);
+            db.SaveChanges();
+            return RedirectToAction("Categories");
+        }
+
     }
 }
