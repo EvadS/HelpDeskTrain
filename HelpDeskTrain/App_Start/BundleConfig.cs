647a763534bd856ebee97f7f4b40e3511eef4d59
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-12 20:33:10 +0300

    начинаем натягивать bootstrap

--- HelpDeskTrain/App_Start/BundleConfig.cs
+++ HelpDeskTrain/App_Start/BundleConfig.cs
@@ -38,7 +38,11 @@ namespace HelpDeskTrain
                         "~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.theme.css"));    
+            
+            bundles.Add(new StyleBundle("~/bundles/bootstrap").Include(
+                      "~/Content/bootstrap.css"));
+
         }
     }
 }
